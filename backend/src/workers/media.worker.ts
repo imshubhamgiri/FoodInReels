0d1589b2462ddbf51f303f6cf56817215b91be13
imshubhamgiri <skgiri569@gmail.com>
2026-06-25 21:29:40 +0530

    refactor(worker): setup mongodb connection and retry logic

--- backend/src/workers/media.worker.ts
+++ backend/src/workers/media.worker.ts
@@ -1,41 +1,38 @@
 import {Worker} from 'bullmq'
-import storageService from '../service/storage.service';
+import dotenv from 'dotenv';
 import { queueConnection }from '../config/queue.config';
-import foodModel from '../models/food.model';
+import { File, IFood } from '../types';
+import{ processBackgroundUpload} from '../services/food.service';
+import mongoose from 'mongoose';
+
+dotenv.config();
+mongoose.set('bufferCommands', false);
+
+// 3. Establish the database connection FOR THIS PROCESS
+const mongoUri = process.env.MONGO_URL || 'mongodb://localhost:27017/zomatoDb';
+mongoose.connect(mongoUri)
+  .then(() => console.log('✅ Worker successfully connected to MongoDB database.' , mongoUri))
+  .catch((err) => console.error('❌ Worker failed to connect to MongoDB:', err))
+console.log('Ready to process background uploads for food items...')
 
 
 const worker = new Worker('videoUpload', async (job) => {
-    const { foodItemId, fileBuffer, fileName, mimeType } = job.data;
-    
-    const file = {
-      fileBuffer: Buffer.from(fileBuffer, 'base64'),
-      fileName,
-      mimeType
-    };
+  console.log(`Processing job ${job.id} for food item: ${job.data.foodItemId}`);
+  const { foodItemId, type, file } = job.data;
   
-    // 1. Run your exact ImageKit upload logic
-    const uploadResult = await storageService.uploadVideo(file);
-    
-    // 2. THE DATABASE PART: Update the document using the ID
-    const updatedItem = await foodModel.findByIdAndUpdate(
-      foodItemId,
-      {
-        videoUrl: uploadResult.url,
-        imageKitFileId: uploadResult.fileId,
-        uploadStatus: "ready" // Update status to ready
-      },
-      { new: true }
-    );
+  // Reconstruct the file buffer structure
+  const fileData:File = {
+    fileBuffer: Buffer.from(file.fileBuffer, 'base64'),
+    fileName: file.fileName,
+    mimeType: file.mimeType
+  };
+  console.log('file Buffer Ready for processing:', fileData.fileBuffer.length, 'bytes')
+
+  // Run the background upload processing service method
+  await processBackgroundUpload(foodItemId, fileData, type);
   
-    // 3. THE NOTIFICATION PART: Tell the client it's done!
-    // Send a real-time event via WebSockets/Socket.io to the user
-        // io.to(updatedItem.partnerId).emit('video_ready', {
-        // foodItemId: updatedItem._id,
-        // url: updatedItem.videoUrl,
-        // message: "Your food item video has been processed successfully!"
-        // });
-  
-  }, { connection: queueConnection, concurrency: 5 });
+  console.log(`Successfully completed upload for food item: ${foodItemId}`);
+}, { connection: queueConnection });
 
 
   worker.on('completed', (job) => {
