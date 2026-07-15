afac7121d07be7e5198b5df7f8d45635821ad6c8
imshubhamgiri <skgiri569@gmail.com>
2026-07-15 21:59:46 +0530

    fix: datadog loading order and http transport

--- backend/tracer.ts
+++ backend/tracer.ts
@@ -3,7 +3,9 @@ import tracer from 'dd-trace';
 
 // Initialize the tracker
 tracer.init({
-  logInjection: true // This connects your server logs to your performance graphs automatically
+  logInjection: true, // This connects your server logs to your performance graphs automatically
+  url:`https://all-http-intake.logs.${process.env.DD_SITE || 'datadoghq.com'}`,
+
 });
 
 export default tracer;
