b6dc6730b607508c3a51daf2a2a99b4e9ce5381a
imshubhamgiri <skgiri569@gmail.com>
2026-07-15 22:21:50 +0530

    update url

--- backend/tracer.ts
+++ backend/tracer.ts
@@ -4,7 +4,7 @@ import tracer from 'dd-trace';
 // Initialize the tracker
 tracer.init({
   logInjection: true, // This connects your server logs to your performance graphs automatically
-  url:`https://all-http-intake.logs.${process.env.DD_SITE || 'datadoghq.com'}`,
+  url:'https://trace.agentless.us5.datadoghq.com',
 
 });
 
